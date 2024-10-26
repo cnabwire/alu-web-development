9e4c4b0e4800513d4a9b5ded285a25d38e478725
cnabwire <chelseaomondi@alustudent.com>
2024-10-26 07:14:18 -0700

    prints a dictionary by ordered keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    keys = []
-    if a_dictionary:
-        for a, b in a_dictionary.items():
-            keys.append(a)
-
-        keys.sort()
-        for items in keys:
-            print("{}: {}".format(items, a_dictionary[items]))
+    return (len(a_dictionary))
