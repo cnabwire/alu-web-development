36fa8f2dc8af755dbf8c4975cac1bec63c82bf4d
cnabwire <chelseaomondi@alustudent.com>
2024-11-09 12:19:52 -0800

    defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a square class"""
+
+
+class Square:
+    """ rep... of square class"""
+
+    def __init__(self, size):
+        # instantation with size for our obect
+        # initializatioin
+        self.__size = size
