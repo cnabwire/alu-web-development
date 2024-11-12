a6aabc9701e9dc14db0d4f781febaca932f3eafc
cnabwire <chelseaomondi@alustudent.com>
2024-11-12 02:30:45 -0800

    A class that inherits from a class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,8 @@ class MyList(list):
     """A class that inherits from list and adds a print_sorted method."""
     
     def print_sorted(self):
-        """Prints the list in ascending sorted order."""
-       if all(isinstance(i, int) for i in self):
-          print(sorted(self))
-       else:
-           print("All elements must be integers")
+        """Prints the list in ascending sorted order, assuming all elements are integers."""
+        if all(isinstance(i, int) for i in self):
+            print(sorted(self))
+        else:
+            print("All elements must be integers.")
