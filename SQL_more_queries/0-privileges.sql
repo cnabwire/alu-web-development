5620691115c7568ab62462559f40ecfcb4d0ad35
cnabwire <chelseaomondi@alustudent.com>
2024-11-18 01:31:55 -0800

    Lists all privilages

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
+-- Script that lists all privileges of the MySQL users
 -- Query to list all privileges (GRANT) of the MySQL users
--- Check privillages for user_0d_1
-SELECT CONCAT('Grants for user_0d_1@localhost') AS '';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Check privillages for user_0d_2
-SELECT CONCAT('Grants for user_0d_2@localhost') AS '';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
