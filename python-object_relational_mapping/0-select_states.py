dc0215ce804a8e9aae53eb8b507de56579f6baba
cnabwire <chelseaomondi@alustudent.com>
2024-11-29 21:33:08 -0800

    Lists all states from the database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Documented"""
+
+import MySQLdb
+import sys
+
+
+def sql_connect(usr, pw, db_name):
+    """This function imports a database to use"""
+
+    """Establish connection"""
+    db = MySQLdb.connect(host="localhost",
+                         user=usr,
+                         port=3306,
+                         passwd=pw,
+                         database=db_name)
+
+    """Create cursor object"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+
+    """Access the queried data to print"""
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    """Close the connection and cursor object"""
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
